c2d63f10f80cabc7fc81ac860d40dc5bc0212c93
HerickSilva9 <149031628+HerickSilva9@users.noreply.github.com>
2025-12-25 17:09:50 -0300

    utiliza variáveis intermediárias para facilitar a criação de symlinks

--- home/dotfiles/default.nix
+++ home/dotfiles/default.nix
@@ -1,20 +1,27 @@
 { config, lib, pkgs, ...}:
 
+let
+
+  home = "${config.home.homeDirectory}";
+  mkOutOfStore = config.lib.file.mkOutOfStoreSymlink;
+
+in
+
 {
 
   # Git
-  home.file.".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "/home/herick/nixos/home/dotfiles/git/.gitconfig";
+  home.file.".gitconfig".source = mkOutOfStore "${home}/nixos/home/dotfiles/git/.gitconfig";
 
   # Kitty
-  home.file.".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "/home/herick/nixos/home/dotfiles/kitty";
+  home.file.".config/kitty".source = mkOutOfStore "${home}/nixos/home/dotfiles/kitty";
 
   # nvim
-  home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/herick/nixos/home/dotfiles/nvim";
+  home.file.".config/nvim".source = mkOutOfStore "${home}/nixos/home/dotfiles/nvim";
 
   # user-dirs
   home.file = {
-      ".config/user-dirs.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/herick/nixos/home/dotfiles/user-dirs/user-dirs.conf";
-      ".config/user-dirs.dirs".source = config.lib.file.mkOutOfStoreSymlink "/home/herick/nixos/home/dotfiles/user-dirs/user-dirs.dirs";
+      ".config/user-dirs.conf".source = mkOutOfStore "${home}/nixos/home/dotfiles/user-dirs/user-dirs.conf";
+      ".config/user-dirs.dirs".source = mkOutOfStore "${home}/nixos/home/dotfiles/user-dirs/user-dirs.dirs";
   };
 
 }
